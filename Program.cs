a4f2c0c5580075dce350e3c646c09d8445b0ae4c
vsolgere <viviansolgere@gmail.com>
2024-01-30 15:49:24 -0700

    printBoard

--- Program.cs
+++ Program.cs
@@ -9,3 +9,4 @@ public string checkWinner(array)
 
 
 */
+
--- TicTacTools.cs
+++ TicTacTools.cs
@@ -1,8 +1,42 @@
 ﻿using System;
 
-public class Class1
+public class TicTacTools
 {
-	public Class1()
+
+	char[] tester = new char[] { 'X', ' ', 'O', 'O', 'X', ' ', ' ', 'O', 'X' };
+
+	static string printBoard(char[] array)
 	{
+		//System.Text.StringBuilder sb = new System.Text.StringBuilder();
+		string board = "";
+
+		//foreach (var item in array)
+		//{
+		//   sb.Append(item.ToString());
+		// sb.Append(item.ToString() + ", ");
+		//}
+
+		for (int x = 0; x < 9, x++)
+		{
+
+			board += array[x];
+
+			if (x == 2 || x == 5)
+			{
+
+				board += "\n";
+			}
+			else
+			{
+				board += " ";
+			}
+		}
+
+		return board.ToString();
 	}
 }
+
+print(printBoard(tester));
+
+// testing
+
