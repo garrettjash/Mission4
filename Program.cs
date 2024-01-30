0842ccb886a8dc7786483743a6614989f58059bf
garrettjash <garrettashcroft@gmail.com>
2024-01-30 15:16:22 -0700

    Description, Method Names

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// Mission 4 - TicTacToe
+// Group 2-2
+// Garrett Ashcroft, Caroline Tobler, Jared Rosenlund, Vivian Solgere
+
+
+/* methods:
+public string printBoard(array)
+public string checkWinner(array)
+
+
+*/
